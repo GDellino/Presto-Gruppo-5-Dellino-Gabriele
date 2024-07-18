c0c835bc02766bb606c2149534f248aadffb862e
Paolo Benincasa <paolobenincasa1984@gmail.com>
2024-07-18 15:42:00 +0200

    media query

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let navbar = document.querySelector("#navbar");
 window.addEventListener("scroll", () =>{
     let scrollValue = window.scrollY;
-    if(scrollValue > 20) {
-        navbar.classList.remove("mt-4", "w-50")
-        navbar.classList.add("w-100")
-    } else{
-        navbar.classList.add("mt-4", "w-50")
-        navbar.classList.remove("w-100")
-    }
+    if(window.innerWidth >600){
+        if(scrollValue > 20) {
+            navbar.classList.remove("mt-4", "w-50")
+            navbar.classList.add("w-100")
+        } else{
+            navbar.classList.add("mt-4", "w-50")
+            navbar.classList.remove("w-100")
+        }
+    } 
+    
 })
